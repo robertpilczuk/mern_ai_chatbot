278795e5c1e9a2adb000478a3363025e6e6a8ad1
robertpilczuk <robertpilczuk@gmail.com>
2023-10-24 14:44:19 +0200

    add validators to users data

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -27,7 +27,7 @@ export const userSignup = async (
         //user signup
         const { name, email, password } = req.body;
         const hashedPassword = await hash(password, 10);
-        const user = new User({ name, email, password: hashedPassword });
+        const user = new User({ name, email, password:hashedPassword });
         await user.save();
         return res.status(200).json({ message: "OK", id:user._id.toString() });
     } catch (error) {

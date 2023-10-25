7437b75cc7ca2a087b018ef8fff24f25bdacd888
robertpilczuk <robertpilczuk@gmail.com>
2023-10-25 12:01:47 +0200

    create toket for auth process

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
-import { hash } from "bcrypt";
+import { hash, compare } from "bcrypt";
+import { createToken } from "../utils/token-manager.js";
 export const getAllUsers = async (req, res, next) => {
     try {
         //get all users
@@ -15,9 +16,32 @@ export const userSignup = async (req, res, next) => {
     try {
         //user signup
         const { name, email, password } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (existingUser)
+            return res.status(401).send("User already registered!");
         const hashedPassword = await hash(password, 10);
         const user = new User({ name, email, password: hashedPassword });
         await user.save();
+        return res.status(201).json({ message: "OK", id: user._id.toString() });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
+};
+export const userLogin = async (req, res, next) => {
+    try {
+        //user login
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).send("User not registered!");
+        }
+        const isPasswordCorrect = await compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(403).send("Incorrect Password");
+        }
+        const token = createToken(user._id.toString(), user.email, "7d");
         return res.status(200).json({ message: "OK", id: user._id.toString() });
     }
     catch (error) {
